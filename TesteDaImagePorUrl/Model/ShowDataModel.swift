787f551a94d3713b1ce6c496c02f47e9c6a87089
Guilherme Takakuwa <guilherme.takakuwa@gmail.com>
2019-03-14 13:24:55 -0300

    estruturação do projeto pt1. criado pasta view

---
+++ TesteDaImagePorUrl/Model/ShowDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  ShowDataModel.swift
+//  EntregaProjeto
+//
+//  Created by Guilherme Takakuwa on 3/14/19.
+//  Copyright © 2019 Guilherme Takakuwa. All rights reserved.
+//
+
+import UIKit
+
+class ShowDataModel{
+    
+    var posterImage:String = ""
+    var showName:String = ""
+    var showGenres:String = ""
+}
--- TesteDaImagePorUrl/ViewController.swift
+++ TesteDaImagePorUrl/ViewController.swift
@@ -7,33 +7,68 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
     
     @IBOutlet weak var backgroudShow: UIImageView!
     @IBOutlet weak var poster1: UIImageView!
-   
     @IBOutlet weak var titulo1: UITextView!
     @IBOutlet weak var genero1: UITextView!
-    @IBOutlet weak var descricao1: UITextView!
-    
 
-    let urlKey = "http://static.tvmaze.com/uploads/images/original_untouched/1/3603.jpg"
+    let showDataModel = ShowDataModel()
+    
+    let api:String = "http://api.tvmaze.com/search/shows?q=rick"
+    var retornoApi:JSON = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       showImage(self)
-        
+//       showImage(self)
+        getAPI(url:api)
+        setInfoShows()
         titulo1.layer.cornerRadius = 15
         genero1.layer.cornerRadius = 15
-        descricao1.layer.cornerRadius = 15
+        
     }
 
-    func showImage(_ sender: Any) {
+    func getAPI(url: String) {
+        Alamofire.request(url, method: .get).responseJSON {
+            response in
+            if response.result.isSuccess {
+                self.retornoApi = JSON(response.result.value!)
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+            }
+        }
+    }
     
-    if let url = URL(string: urlKey){
-        
+    func updateUserUI() {
+        setImage(self, api: showDataModel.posterImage)
+        titulo1.text = showDataModel.showName
+        genero1.text = showDataModel.showGenres
+    }
+    
+    func setInfoShows () {
+        if  let posterImage:String = self.retornoApi[0]["show"]["image"]["medium"].stringValue {
+            print(self.retornoApi)
+            print("entrou")
+            showDataModel.posterImage =  self.retornoApi[0]["show"]["image"]["medium"].stringValue
+            print(showDataModel.posterImage)
+            showDataModel.showGenres = self.retornoApi[0]["show"]["genres"][0].stringValue
+            print(showDataModel.showGenres)
+            showDataModel.showName = self.retornoApi[0]["show"]["name"].stringValue
+            print(showDataModel.showName)
+            updateUserUI()
+        }
+    }
+    
+    
+    func setImage(_ sender: Any, api:String) {
+    
+    if let url = URL(string:api ){
         do {
             let data = try Data(contentsOf: url)
             self.poster1.image = UIImage(data: data)
